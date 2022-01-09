30a1b361bfb0e1355d1c2fc2959d0d6376f24acf
ishashukla183 <93022787+ishashukla183@users.noreply.github.com>
2022-01-09 15:29:49 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+//typing animation JavaScript
+
+
+var typed = new Typed(".typing", {
+  strings : ["a programmer" , "a developer", "an ML enthusiast"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+});
